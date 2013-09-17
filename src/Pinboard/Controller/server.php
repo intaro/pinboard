e19db55728ed6ba00ccd8ebc8d42b0186e7d6d22
Ilyas Salikhov <salikhoff@gmail.com>
2013-09-17 15:49:30 +0400

    fix #37

--- src/Pinboard/Controller/server.php
+++ src/Pinboard/Controller/server.php
@@ -15,7 +15,7 @@ function checkUserAccess($app, $serverName) {
         if ($app['params']['secure']['enable'] == "true") {
             $user = $app['security']->getToken()->getUser();
             $hostsRegExp = isset($app['params']['secure']['users'][$user->getUsername()]['hosts'])
-                        ? $app['params']['secure']['users'][$user->getUsername()]['hosts'] 
+                        ? $app['params']['secure']['users'][$user->getUsername()]['hosts']
                         : ".*";
             if (trim($hostsRegExp) == "") {
                 $hosts = ".*";
@@ -32,21 +32,21 @@ $server->get('/{serverName}/{hostName}/overview.{format}', function($serverName,
     checkUserAccess($app, $serverName);
 
     $result = array();
-    $result['hosts']       = getHosts($app['db'], $serverName);    
+    $result['hosts']       = getHosts($app['db'], $serverName);
     $result['req']         = getRequestReview($app['db'], $serverName, $hostName);
     $result['req_per_sec'] = getRequestPerSecReview($app['db'], $serverName, $hostName);
     $result['statuses']    = getStatusesReview($app['db'], $serverName, $hostName);
 
-    if ($format == 'html') {        
+    if ($format == 'html') {
         $result['server_name'] = $serverName;
         $result['hostname'] = $hostName;
         $result['title'] = $serverName;
 
         return $app['twig']->render(
-            'server.html.twig', 
+            'server.html.twig',
             $result
         );
-    } 
+    }
     if ($format == 'json') {
         unset($result['hosts']);
         foreach ($result['req'] as &$value) {
@@ -87,7 +87,7 @@ function getHosts($conn, $serverName) {
     while ($data = $stmt->fetch()) {
       $hosts[] = $data['hostname'];
     }
-    
+
     return $hosts;
 }
 
@@ -97,12 +97,12 @@ function getStatusesReview($conn, $serverName, $hostName) {
         'created_at'  => date('Y-m-d H:i:s', strtotime('-1 day')),
     );
     $hostCondition = '';
-    
+
     if ($hostName != 'all') {
         $params['hostname'] = $hostName;
         $hostCondition = 'AND hostname = :hostname';
     }
-    
+
     $sql = '
         SELECT
             created_at, status, count(*) as cnt
@@ -117,9 +117,9 @@ function getStatusesReview($conn, $serverName, $hostName) {
         ORDER BY
             created_at
     ';
-    
+
     $stmt = $conn->executeQuery($sql, $params);
-    
+
     $statuses = array(
         'data'  => array(),
         'codes' => array(),
@@ -127,7 +127,7 @@ function getStatusesReview($conn, $serverName, $hostName) {
     while ($data = $stmt->fetch()) {
         $t = strtotime($data['created_at']);
         $date = date('Y,', $t) . (date('n', $t) - 1) . date(',d,H,i', $t);
-        
+
         $statuses['data'][] = array(
             'created_at' => $data['created_at'],
             'date' => $date,
@@ -138,7 +138,7 @@ function getStatusesReview($conn, $serverName, $hostName) {
             //set color
             $statuses['codes'][$data['status']] = Utils::generateColor();
         }
-    }          
+    }
     ksort($statuses['codes']);
 
     return $statuses;
@@ -151,13 +151,13 @@ function getRequestPerSecReview($conn, $serverName, $hostName) {
     );
     $table = 'ipm_report_by_server_name';
     $hostCondition = '';
-    
+
     if ($hostName != 'all') {
         $params['hostname'] = $hostName;
         $table = 'ipm_report_by_hostname_and_server';
         $hostCondition = 'AND hostname = :hostname';
     }
-    
+
     $sql = '
         SELECT
             created_at, req_per_sec
@@ -170,9 +170,9 @@ function getRequestPerSecReview($conn, $serverName, $hostName) {
         ORDER BY
             created_at
     ';
-    
+
     $data = $conn->fetchAll($sql, $params);
-    
+
     foreach($data as &$item) {
         $t = strtotime($item['created_at']);
         $item['date'] = date('Y,', $t) . (date('n', $t) - 1) . date(',d,H,i', $t);
@@ -189,16 +189,16 @@ function getRequestReview($conn, $serverName, $hostName) {
     );
     $hostCondition = '';
     $index = 'sn_c';
-    
+
     if ($hostName != 'all') {
         $params['hostname'] = $hostName;
         $hostCondition = 'AND hostname = :hostname';
         $index = 'sn_h_c';
     }
-    
+
     $sql = '
         SELECT
-            created_at, 
+            created_at,
             req_time_90, req_time_95, req_time_99, req_time_100,
             mem_peak_usage_90, mem_peak_usage_95, mem_peak_usage_99, mem_peak_usage_100
         FROM
@@ -212,9 +212,9 @@ function getRequestReview($conn, $serverName, $hostName) {
         ORDER BY
             created_at
     ';
-    
+
     $data = $conn->fetchAll($sql, $params);
-    
+
     foreach($data as &$item) {
         $t = strtotime($item['created_at']);
         $item['date'] = date('Y,', $t) . (date('n', $t) - 1) . date(',d,H,i', $t);
@@ -243,7 +243,7 @@ $server->get('/{serverName}/{hostName}/statuses/{pageNum}', function($serverName
         'title'       => 'Error pages / ' . $serverName,
         'pageNum'     => $pageNum,
     );
-    
+
     $result['rowPerPage'] = $rowPerPage;
 
     $rowCount = getErrorPagesCount($app['db'], $serverName, $hostName);
@@ -256,14 +256,14 @@ $server->get('/{serverName}/{hostName}/statuses/{pageNum}', function($serverName
         if ($pageNum < 1 || $pageNum > $pageCount) {
             $app->abort(404, "Page $pageNum does not exist.");
         }
-    }   
+    }
 
     $startPos = ($pageNum - 1) * $rowPerPage;
-    $result['hosts']    = getHosts($app['db'], $serverName);    
+    $result['hosts']    = getHosts($app['db'], $serverName);
     $result['statuses'] = getErrorPages($app['db'], $serverName, $hostName, $startPos, $rowPerPage);
 
     return $app['twig']->render(
-        'statuses.html.twig', 
+        'statuses.html.twig',
         $result
     );
 })
@@ -278,7 +278,7 @@ function getErrorPagesCount($conn, $serverName, $hostName) {
         'created_at'  => date('Y-m-d H:i:s', strtotime('-1 day')),
     );
     $hostCondition = '';
-    
+
     if ($hostName != 'all') {
         $params['hostname'] = $hostName;
         $hostCondition = 'AND hostname = :hostname';
@@ -306,7 +306,7 @@ function getErrorPages($conn, $serverName, $hostName, $startPos, $rowCount) {
         'created_at'  => date('Y-m-d H:i:s', strtotime('-1 day')),
     );
     $hostCondition = '';
-    
+
     if ($hostName != 'all') {
         $params['hostname'] = $hostName;
         $hostCondition = 'AND hostname = :hostname';
@@ -328,15 +328,15 @@ function getErrorPages($conn, $serverName, $hostName, $startPos, $rowCount) {
     ';
 
     $data = $conn->fetchAll($sql, $params);
-    
+
     return $data;
 }
 
 $server->get('/{serverName}/{hostName}/req-time/{pageNum}', function($serverName, $hostName, $pageNum) use ($app, $rowPerPage) {
     checkUserAccess($app, $serverName);
-    
+
     $pageNum = str_replace('page', '', $pageNum);
-    
+
     $result = array(
         'server_name' => $serverName,
         'hostname'    => $hostName,
@@ -358,11 +358,11 @@ $server->get('/{serverName}/{hostName}/req-time/{pageNum}', function($serverName
     }
     $startPos = ($pageNum - 1) * $rowPerPage;
 
-    $result['hosts'] = getHosts($app['db'], $serverName);    
+    $result['hosts'] = getHosts($app['db'], $serverName);
     $result['pages'] = getSlowPages($app['db'], $serverName, $hostName, $startPos, $rowPerPage);
 
     return $app['twig']->render(
-        'req_time.html.twig', 
+        'req_time.html.twig',
         $result
     );
 })
@@ -377,7 +377,7 @@ function getSlowPagesCount($conn, $serverName, $hostName) {
         'created_at'  => date('Y-m-d H:i:s', strtotime('-1 day')),
     );
     $hostCondition = '';
-    
+
     if ($hostName != 'all') {
         $params['hostname'] = $hostName;
         $hostCondition = 'AND hostname = :hostname';
@@ -405,7 +405,7 @@ function getSlowPages($conn, $serverName, $hostName, $startPos, $rowCount) {
         'created_at'  => date('Y-m-d H:i:s', strtotime('-1 day')),
     );
     $hostCondition = '';
-    
+
     if ($hostName != 'all') {
         $params['hostname'] = $hostName;
         $hostCondition = 'AND hostname = :hostname';
@@ -427,26 +427,26 @@ function getSlowPages($conn, $serverName, $hostName, $startPos, $rowCount) {
     ';
 
     $data = $conn->fetchAll($sql, $params);
-    
+
     foreach($data as &$item) {
         $item['req_time']  = number_format($item['req_time'] * 1000, 0, '.', ',');
     }
-    
+
     return $data;
 }
 
 $server->get('/{serverName}/{hostName}/mem-usage/{pageNum}', function($serverName, $hostName, $pageNum) use ($app, $rowPerPage) {
     checkUserAccess($app, $serverName);
-    
+
     $pageNum = str_replace('page', '', $pageNum);
-    
+
     $result = array(
         'server_name' => $serverName,
         'hostname'    => $hostName,
         'title'       => 'Memory peak usage / ' . $serverName,
         'pageNum'     => $pageNum,
     );
-    
+
     $result['rowPerPage'] = $rowPerPage;
 
     $rowCount = getHeavyPagesCount($app['db'], $serverName, $hostName);
@@ -461,11 +461,11 @@ $server->get('/{serverName}/{hostName}/mem-usage/{pageNum}', function($serverNam
     }
     $startPos = ($pageNum - 1) * $rowPerPage;
 
-    $result['hosts'] = getHosts($app['db'], $serverName);    
+    $result['hosts'] = getHosts($app['db'], $serverName);
     $result['pages'] = getHeavyPages($app['db'], $serverName, $hostName, $startPos, $rowPerPage);
 
     return $app['twig']->render(
-        'mem_usage.html.twig', 
+        'mem_usage.html.twig',
         $result
     );
 })
@@ -480,7 +480,7 @@ function getHeavyPagesCount($conn, $serverName, $hostName){
         'created_at'  => date('Y-m-d H:i:s', strtotime('-1 day')),
     );
     $hostCondition = '';
-    
+
     if ($hostName != 'all') {
         $params['hostname'] = $hostName;
         $hostCondition = 'AND hostname = :hostname';
@@ -508,7 +508,7 @@ function getHeavyPages($conn, $serverName, $hostName, $startPos, $rowCount) {
         'created_at'  => date('Y-m-d H:i:s', strtotime('-1 day')),
     );
     $hostCondition = '';
-    
+
     if ($hostName != 'all') {
         $params['hostname'] = $hostName;
         $hostCondition = 'AND hostname = :hostname';
@@ -530,22 +530,22 @@ function getHeavyPages($conn, $serverName, $hostName, $startPos, $rowCount) {
     ';
 
     $data = $conn->fetchAll($sql, $params);
-    
+
     foreach($data as &$item) {
         $item['mem_peak_usage']  = number_format($item['mem_peak_usage'], 0, '.', ',');
     }
-    
+
     return $data;
 }
 
 $server->get('/{serverName}/{hostName}/live', function(Request $request, $serverName, $hostName) use ($app) {
     checkUserAccess($app, $serverName);
-    
+
     if ($request->isXmlHttpRequest()) {
         $result = array(
             'pages' => getLivePages($app['db'], $serverName, $hostName, $request->get('last_id')),
         );
-        
+
         return $app->json($result);
     }
 
@@ -555,13 +555,13 @@ $server->get('/{serverName}/{hostName}/live', function(Request $request, $server
         'title'       => 'Live / ' . $serverName,
         'limit'       => 100,
     );
-    
-    $result['hosts'] = getHosts($app['db'], $serverName);    
+
+    $result['hosts'] = getHosts($app['db'], $serverName);
     $result['pages'] = getLivePages($app['db'], $serverName, $hostName, null, $result['limit']);
     $result['last_id'] = sizeof($result['pages']) ? $result['pages'][0]['id'] : 0;
-        
+
     return $app['twig']->render(
-        'live.html.twig', 
+        'live.html.twig',
         $result
     );
 })
@@ -575,7 +575,7 @@ function getLivePages($conn, $serverName, $hostName, $lastId = null, $limit = 50
     );
     $hostCondition = '';
     $idCondition   = '';
-    
+
     if ($hostName != 'all') {
         $params['hostname'] = $hostName;
         $hostCondition = 'AND hostname = :hostname';
@@ -587,7 +587,7 @@ function getLivePages($conn, $serverName, $hostName, $lastId = null, $limit = 50
 
     $sql = '
         SELECT
-            id, server_name, hostname, script_name, req_time, mem_peak_usage
+            id, server_name, hostname, script_name, req_time, status, mem_peak_usage
         FROM
             request
         WHERE
@@ -601,12 +601,12 @@ function getLivePages($conn, $serverName, $hostName, $lastId = null, $limit = 50
     ';
 
     $data = $conn->fetchAll($sql, $params);
-    
+
     foreach($data as &$item) {
         $item['req_time']        = number_format($item['req_time'] * 1000, 0, '.', ',');
         $item['mem_peak_usage']  = $item['mem_peak_usage'];
     }
-    
+
     return $data;
 }
 
