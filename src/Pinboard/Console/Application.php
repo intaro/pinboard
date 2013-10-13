b39feb033a09584be4f8fc203485a5039b5542c1
Ilyas Salikhov <salikhoff@gmail.com>
2013-10-13 15:45:02 +0400

    fix #45 - added migrations, removed unused tables

--- src/Pinboard/Console/Application.php
+++ src/Pinboard/Console/Application.php
@@ -6,12 +6,12 @@ use Symfony\Component\Console\Application as ConsoleApplication;
 class Application extends ConsoleApplication
 {
     protected $app = null;
-    
+
     public function setSilex($app)
     {
         $this->app = $app;
     }
-    
+
     public function getSilex()
     {
         return $this->app;
