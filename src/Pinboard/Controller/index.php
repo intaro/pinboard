b7d57366258a07855d23f643b82f57435e702f64
Ilyas Salikhov <salikhoff@gmail.com>
2014-05-22 19:41:42 +0400

    fix #73

--- src/Pinboard/Controller/index.php
+++ src/Pinboard/Controller/index.php
@@ -6,7 +6,7 @@ use Pinboard\Utils\IDNaConvert;
 $index = $app['controllers_factory'];
 
 $index->get('/', function() use ($app) {
-    
+
     $result = array();
 
     $hosts = ".*";
@@ -15,7 +15,7 @@ $index->get('/', function() use ($app) {
         if ($app['params']['secure']['enable'] == "true") {
             $user = $app['security']->getToken()->getUser();
             $hosts = isset($app['params']['secure']['users'][$user->getUsername()]['hosts'])
-                        ? $app['params']['secure']['users'][$user->getUsername()]['hosts'] 
+                        ? $app['params']['secure']['users'][$user->getUsername()]['hosts']
                         : ".*";
             if (trim($hosts) == "") {
                 $hosts = ".*";
@@ -27,20 +27,20 @@ $index->get('/', function() use ($app) {
         'created_at' => date('Y-m-d H:i:s', strtotime('-1 day')),
         'hosts' => $hosts,
     );
-    
+
     $sql = '
         SELECT
-            a.server_name, 
-            sum(a.req_count) as req_count, 
-            avg(a.req_per_sec) as req_per_sec, 
+            a.server_name,
+            sum(a.req_count) as req_count,
+            avg(a.req_per_sec) as req_per_sec,
             (
-                SELECT 
-                    count(*) 
-                FROM 
-                    ipm_status_details b 
-                WHERE 
+                SELECT
+                    count(*)
+                FROM
+                    ipm_status_details b
+                WHERE
                     a.server_name = b.server_name AND b.created_at > :created_at
-            ) 
+            )
             as error_count
         FROM
             ipm_report_by_hostname_and_server a
@@ -49,21 +49,30 @@ $index->get('/', function() use ($app) {
         GROUP BY
             a.server_name
     ';
-    
+
     $stmt = $app['db']->executeQuery($sql, $params, array(), new QueryCacheProfile(5 * 60));
     $result['servers'] = $stmt->fetchAll();
     $stmt->closeCursor();
 
     $idn = new IDNaConvert(array('idn_version' => 2008));
 
+    $total = array(
+        'req_count' => 0,
+        'error_count' => 0,
+    );
     foreach($result['servers'] as &$item) {
         if (stripos($item['server_name'], 'xn--') !== false) {
             $item['server_name'] = $idn->decode($item['server_name']);
         }
 
         $item['req_per_sec'] = number_format($item['req_per_sec'], 3, ',', '');
+
+        $total['req_count'] += $item['req_count'];
+        $total['error_count'] += $item['error_count'];
     }
-    
+
+    $result['total'] = $total;
+
     return $app['twig']->render('index.html.twig', $result);
 })
 ->bind('index');
