f34eb9b6d413dc7bf487f2b40066cf218c106077
ali <koderoff@gmail.com>
2014-10-18 15:40:46 +0300

    migration fix

--- src/Pinboard/DoctrineMigrations/Version20141018115941.php
+++ src/Pinboard/DoctrineMigrations/Version20141018115941.php
@@ -12,17 +12,13 @@ class Version20141018115941 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql(
-            "ALTER TABLE `ipm_status_details` ADD INDEX `isd_hostname` (`hostname`);" .
-            "ALTER TABLE `ipm_status_details` ADD INDEX `isd_status` (`status`);"
-        );
+        $this->addSql("ALTER TABLE `ipm_status_details` ADD INDEX `isd_hostname` (`hostname`);");
+        $this->addSql("ALTER TABLE `ipm_status_details` ADD INDEX `isd_status` (`status`);");
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql(
-            "ALTER TABLE `ipm_status_details` DROP INDEX `isd_hostname`;" .
-            "ALTER TABLE `ipm_status_details` DROP INDEX `isd_status`;"
-        );
+        $this->addSql("ALTER TABLE `ipm_status_details` DROP INDEX `isd_hostname`;");
+        $this->addSql("ALTER TABLE `ipm_status_details` DROP INDEX `isd_status`;");
     }
 }
